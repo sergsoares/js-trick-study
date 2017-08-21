63e18b99b0b47dce3ccd7a68000ce229b021ff41
Sergio <acad.sergio@gmail.com>
2017-08-21 13:12:19 -0300

    Short Circuit Evaluation Shorthand

--- 2-Short-circuit-Evaluation-Shorthand.js
+++ 2-Short-circuit-Evaluation-Shorthand.js
@@ -1,1 +1,25 @@
-//2. Short-circuit Evaluation Shorthand
+/* Explanation 
+    In js we need validate null, undefined and '' (empty String) for it we can use Short Circuit Evaluation
+*/
+
+console.log("2. Short-circuit Evaluation Shorthand")
+
+var answer;
+
+if(answer !== null || answer !== undefined || answer !== ''){
+    var message = answer;
+}
+
+console.log(message)
+
+/****************************************************************/
+console.log("2. Short-circuit Evaluation Shorthand - ES6")
+
+const falsy = [false, null, undefined, 0, NaN, '', ""]
+var answer2;
+const message2 = answer2 || 'nothing'
+
+console.log(message2)
+
+
+
