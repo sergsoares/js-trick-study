32c429ae9670d082011540660c16d29be0b4567a
Sergio <acad.sergio@gmail.com>
2017-08-25 08:28:04 -0300

    Fany way to represent zeros

---
+++ 7-Decimal-base-exponents.js
@@ -0,0 +1,16 @@
+/**
+ * Explanation : We can represents numbers without the trailing zeros, 1e4 will be equals 10000
+ */
+
+console.log("7. Decimal base exponents")
+
+for(var i = 0; i < 10000; i++){
+    console.log(i)
+}
+/****************************************************************/
+
+console.log("7. Decimal base exponents - ShortHand")
+
+for(var i = 0; i < 1e4; i++){
+    console.log(i)
+}
